41c53badfeb17acce46bb7424817327e1bcda1c2
NilValue <dawnofchaos33@gmail.com>
2015-03-01 18:22:59 +0100

    master

---
+++ ChessConstellationMap/src/Pieces/ChessPiece.java
@@ -0,0 +1,13 @@
+package Pieces;
+
+public abstract class ChessPiece {
+
+    // Attributes
+    private boolean hasMoved;
+    private Color color;
+
+    // Nonabstract methods
+
+    // Abstract methods
+    public abstract void move();
+}
